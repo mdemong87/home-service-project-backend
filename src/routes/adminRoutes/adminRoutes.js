7d2ab9bed9eff05b09d208b9ed756094a84afe55
Md Emon Hossen <mdemong87@gmail.com>
2025-12-17 16:20:57 +0600

    updated credit point system

--- src/routes/adminRoutes/adminRoutes.js
+++ src/routes/adminRoutes/adminRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { addArea, addCatagory, adminDeshboard, deleteArea, deleteCatagory, getAllAreas, getAllCatagory, updateArea, updateCatagory } from "../../controllers/adminController/adminController.js";
+import { getAdminAllPoints } from "../../controllers/adminController/getAllPoints.js";
 import { authorize, protect } from "../../middlewares/authMiddleware.js";
 
 
@@ -23,6 +24,8 @@ router.put("/updatearea/:id", protect, authorize("admin"), updateArea);
 router.delete("/deletecatagory/:id", protect, authorize("admin"), deleteCatagory);
 
 router.delete("/deletearea/:id", protect, authorize("admin"), deleteArea);
+
+router.get("/allcredits", getAdminAllPoints);
 
 
 
